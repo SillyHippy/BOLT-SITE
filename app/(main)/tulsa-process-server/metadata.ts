21df09f69a43d3fde85485840a19b7587c202f5a
SillyHippy <159052837+SillyHippy@users.noreply.github.com>
2025-08-02 23:08:14 -0500

    Update metadata.ts

--- app/(main)/tulsa-process-server/metadata.ts
+++ app/(main)/tulsa-process-server/metadata.ts
@@ -2,23 +2,23 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Tulsa Process Server | Process Server in Tulsa & Broken Arrow, OK',
-  description: 'Need a reliable Tulsa process server or Broken Arrow processor? We offer fast, licensed, and affordable service across Tulsa County. Get a quote from the top process server in Tulsa today.',
+  description: 'Need a reliable Tulsa process server or Broken Arrow Process Server? We offer fast, licensed, and affordable service across Tulsa County. Get a quote from the top process server in Tulsa today.',
   keywords: [
     'tulsa process server',
     'process server tulsa',
     'process server in tulsa',
-    'processor for tulsa county',
+    'Process Server for tulsa county',
     'tulsa county process server',
     'broken arrow process server',
     'process server broken arrow',
-    'broken arrow processor',
-    'processor for broken arrow',
+    'broken arrow Process Server',
+    'Process Server for broken arrow',
     'process server in broken arrow',
     'broken arrow process servers',
     'process servers broken arrow',
     'broken arrow, ok process server',
     'process server in broken arrow ok',
-    'broken arrow ok processor',
+    'broken arrow ok Process Server',
   ],
   openGraph: {
     title: 'Tulsa Process Server | Process Server in Tulsa & Broken Arrow, OK',
