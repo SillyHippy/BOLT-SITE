879d8332a48d54a7576c8d86f56a76bf6ae04a58
jojo8809 <159052837+SillyHippy@users.noreply.github.com>
2025-08-10 17:34:43 -0500

    fd

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -8,14 +8,14 @@ import {
   ControllerProps,
   FieldPath,
   FieldValues,
-  FormProvider,
+  FormProvider as RHFFormProvider,
   useFormContext,
 } from 'react-hook-form';
 
 import { cn } from '@/lib/utils';
 import { Label } from '@/components/ui/label';
 
-const Form = FormProvider;
+const Form = RHFFormProvider;
 
 type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
