a1d2ba8e02f91bba12663c7afbaf0347989b7706
jojo8809 <159052837+SillyHippy@users.noreply.github.com>
2026-02-22 16:05:37 -0600

    Update instrumentation-client.ts

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -7,4 +7,6 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   capture_pageview: true,
   capture_pageleave: true,
   enable_heatmaps: true,
+  capture_scroll_depth: true,
+  autocapture: true,
 })
