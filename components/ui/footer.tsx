d072956db48a046ebb0693b39b468472f0516251
SillyHippy <159052837+SillyHippy@users.noreply.github.com>
2026-04-11 05:32:06 -0500

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -793,12 +793,6 @@ export function Footer() {
                     Video Hub
                   </a>
                   <a
-                    href="/service-time-estimator"
-                    className="text-blue-600 hover:text-blue-800 underline font-semibold text-sm"
-                  >
-                    Service Time Estimator
-                  </a>
-                  <a
                     href="/service-checker"
                     className="text-blue-600 hover:text-blue-800 underline font-semibold text-sm"
                   >
